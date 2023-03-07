c5347c190468d225e145a4574e19b6cd1b92e170
Julien Geneste <julien@Juliens-MacBook-Pro.local>
2023-03-07 17:38:08 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+sudo apt install curl cmake libcurl4 libcurl4-openssl-dev sudo apt-get install libboost1.74-all-dev -y
+cd ~
+wget https://github.com/libcpr/cpr/archive/refs/tags/1.10.1.zip
+unzip 1.10.1.zip
+cd cpr-1.10.1/
+mkdir build && cd build
+cmake .. -DCPR_USE_SYSTEM_CURL=ON
+cmake --build .
+sudo cmake --install .
+cd ~
+git clone https://github.com/offa/influxdb-cxx
+cd ~/influxdb-cxx/
+mkdir build && cd build
+cmake -D INFLUXCXX_TESTING:BOOL=OFF ..
+sudo make install
+cd ~
+rm -rf ~/influxdb-cxx
+rm -rf ~/cpr-1.10.1 && rm -rf 1.10.1.zip
